114bdca5482b30ed92ce91b25ac4da623f709274
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-07-19 19:02:03 +0200

    fix ssl

--- config/config.js
+++ config/config.js
@@ -15,6 +15,12 @@ module.exports = {
     "production": {
         use_env_variable: "DATABASE_URL",
         url: process.env.DATABASE_URL,
-        "dialect": "postgres"
+        "dialect": "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     },
 }
