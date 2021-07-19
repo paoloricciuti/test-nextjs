61e420ffd6e3cdb4d810d799e4eb663ed29f95bf
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-07-19 19:09:37 +0200

    Fix model

--- pages/api/testdb.js
+++ pages/api/testdb.js
@@ -1,9 +1,9 @@
 export default async (req, res) => {
-    const { sequelize } = req.db;
+    const { rooms } = req.db;
     if (req.method === 'POST') {
-        let inserted = await sequelize.rooms.create(JSON.parse(req.body));
+        let inserted = await rooms.create(JSON.parse(req.body));
         return res.json(inserted);
     } else {
-        return res.json(await sequelize.rooms.findAll());
+        return res.json(await rooms.findAll());
     }
 }
